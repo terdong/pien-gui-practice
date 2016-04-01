8e4dbc5a8004866a1da83722926d8e54f35f3a82
DongHee Kim <donghee@piensoft.co.kr>
2016-04-01 18:41:04 +0900

    1. 카드 덱 2번째 컨텐츠 작업중

---
+++ Pien_New_UI/Assets/Scripts/Tab_01_Deck/CardDeck.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using uTools;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CardDeck : MonoBehaviour {
+    public uTweenPosition Tab_Tween_Position_;
+    public ToggleGroup Tab_Toggle_Group_;
+
+    public void MoveTabCover(Toggle t)
+    {
+        if(t.isOn)
+        {
+            Tab_Tween_Position_.to = t.GetComponent<RectTransform>().anchoredPosition;
+            Tab_Tween_Position_.enabled = true;
+        }else
+        {
+            Tab_Tween_Position_.from = t.GetComponent<RectTransform>().anchoredPosition;
+            Tab_Tween_Position_.ResetToBeginning();
+        }
+    }
+
+    void Awake()
+    {
+
+    }
+}
